910330640b201ca7954616ae09076211d6abba8e
Arthur Carabott <arthurc@gmail.com>
2016-08-15 12:20:47 -0700

    setup gulp environment

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var connect = require('gulp-connect');
+
+gulp.task('babel', function() {
+  return gulp.src(['src/*.js'])
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .on('error', function(error) {
+      console.log('error!');
+      console.log(error.message);
+      this.emit('end');
+    })
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dst'))
+    .pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+  connect.server({ livereload: true });
+});
+
+gulp.task('watchjs', function() {
+  gulp.watch(['src/*.js'], ['babel']);
+});
+
+gulp.task('default', ['watchjs', 'connect']);
