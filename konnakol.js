d047b8e76c15b02eff94f65265f130924ddc23cd
Arthur Carabott <arthurc@gmail.com>
2016-08-15 12:08:56 -0700

    basic outline of playing a phrase

--- konnakol.js
+++ konnakol.js
@@ -1,41 +1,56 @@
-var g = ohm.grammarFromScriptElement();
+var grammar = ohm.grammarFromScriptElement();
 
 var examples = [
   'ta', 'taka', 'takadimi', 'dadiginadom', 'Takadimi', 'da,di,gi,nakadom,'
 ];
 
 examples.forEach(function(example) {
-  console.log(example, g.match(example).succeeded());
+  console.log(example, grammar.match(example).succeeded());
 });
 
 
-// var s = g.createSemantics();
+var semantics = grammar.createSemantics();
 
-// function createSyllable (consonant, vowel, extensions, stress) {
-//   return {
-//     syllable: consonant.sourceString + vowel.sourceString,
-//     duration: 1 + extensions.length(),
-//     stress: stress
-//   };
-// }
+function createSyllable (consonant, vowel, extensions, stress) {
+  return {
+    syllable: consonant.sourceString + vowel.sourceString,
+    duration: 1 + extensions.interpret().length,
+    stress: stress
+  };
+}
 
-// s.addOperation('interpret', {
-//   Phrase: function(words) {
-//     return words;
-//   },
-//   word: function(syllables) {
-//     return syllables;
-//   },
-//   syllable_simple: function(consonant, vowel, extensions) {
-//     return createSyllable(consonant, vowel, extensions, false);
-//   },
-//   syllable_stressed: function(consonant, vowel, extensions) {
-//     return createSyllable(consonant, vowel, extensions, true);
-//   },
-//   extension_extend: function(e) {
-//     return e.interpret();
-//   },
-//   extension_rest: function(e) {
-//     return e.interpret();
-//   }
-// });
+function createPhrase (words) {
+  return {
+    words: words,
+    play: function() {
+      console.log(words);
+      return words;
+    }
+  }
+}
+
+semantics.addOperation('interpret', {
+  Phrase: function(words) {
+    return createPhrase(words.interpret());
+  },
+  word: function(syllables) {
+    return syllables.interpret();
+  },
+  syllable_simple: function(consonant, vowel, extensions) {
+    return createSyllable(consonant, vowel, extensions, false);
+  },
+  syllable_stressed: function(consonant, vowel, extensions) {
+    return createSyllable(consonant, vowel, extensions, true);
+  },
+  extension_extend: function(exp) {
+    return exp.interpret();
+  },
+  extension_rest: function(exp) {
+    return exp.interpret();
+  }
+});
+
+var result = grammar.match('ta');
+var node = semantics(result);
+var phrase = node.interpret();
+console.log(phrase.play());
