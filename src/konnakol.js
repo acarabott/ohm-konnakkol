bbeedfd710bea91441615fae1079b6b6b24f63d5
Arthur Carabott <arthurc@gmail.com>
2016-08-16 08:13:27 -0700

    creating sound library

--- src/konnakol.js
+++ src/konnakol.js
@@ -1,15 +1,34 @@
 const grammar = ohm.grammarFromScriptElement();
 
-const examples = [
-  'ta', 'taka', 'takadimi', 'dadiginadom', 'Takadimi', 'da,di,gi,nakadom,'
-];
+const soundLibraryFactory = (_lookup) => {
+  const lookup = _lookup;
 
-examples.forEach(example => {
-  console.log(example, grammar.match(example).succeeded());
-});
+  if (!lookup.hasOwnProperty('default')) {
+    throw Error('sound library needs to have default sounds set');
+  }
 
+  ['normal', 'stress'].forEach(type => {
+    if (!lookup.default.hasOwnProperty(type)) {
+      throw Error(`sound library default needs a sound for ${type}`);
+    }
+  })
 
-const semantics = grammar.createSemantics();
+  return {
+    get: (syllable, type) => {
+      const haveSound = lookup.hasOwnProperty(syllable) &&
+                        lookup[syllable].hasOwnProperty(type);
+
+      if (haveSound) { return lookup[syllable][type]; }
+
+      const defaultHasType = lookup.default.hasOwnProperty(type);
+      if (defaultHasType) { return lookup.default[type]; }
+
+      console.log(`warning: no sound for [${syllable}][${type}], using default`);
+      return lookup.default['normal'];
+    },
+    set: (syllable, type, buffer) => lookup[syllable][type] = buffer
+  }
+};
 
 function createSyllable (consonant, vowel, extensions, stress) {
   return {
@@ -50,7 +69,36 @@ semantics.addOperation('interpret', {
   }
 });
 
-const result = grammar.match('ta');
-const node = semantics(result);
-const phrase = node.interpret();
-console.log(phrase.play());
+const examples = [
+  'ta', 'taka', 'takadimi', 'dadiginadom', 'Takadimi', 'da,di,gi,nakadom,'
+];
+
+examples.forEach(example => {
+  console.log(example, grammar.match(example).succeeded());
+});
+
+function main (soundLibrary) {
+  const result = grammar.match('takadimi takajuna');
+  const node = semantics(result);
+  const phrase = node.interpret();
+
+  console.log("soundLibrary.get('default', 'normal'):", soundLibrary.get('default', 'normal'));
+  phrase.play();
+}
+
+function setup() {
+  const audioPath = 'assets/audio/';
+  const audioFiles = {'normal': 'normal.mp3', 'stress': 'stress.mp3'};
+  const soundLibraryLookup = { default: {} };
+  const soundFilePromises = Object.keys(audioFiles).map(key => {
+    return loadAudio(`${audioPath}${audioFiles[key]}`).then(buffer => {
+      soundLibraryLookup.default[key] = buffer;
+    });
+  });
+
+  Promise.all(soundFilePromises).then(() => {
+    main(soundLibraryFactory(soundLibraryLookup));
+  });
+}
+
+setup();
