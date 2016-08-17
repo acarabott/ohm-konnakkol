71a27d9f79ed5662d16569e633fecd1d19e52067
Arthur Carabott <arthurc@gmail.com>
2016-08-17 10:02:37 -0700

    setup chrome gulp tasks and structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,26 @@ var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
 var connect = require('gulp-connect');
 
-gulp.task('babel', function() {
-  return gulp.src(['src/*.js'])
-    .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .on('error', function(error) {
-      console.log('error!');
-      console.log(error.message);
-      this.emit('end');
-    })
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dst'))
-    .pipe(connect.reload());
-});
+function babelFactory (src, dst) {
+  return function() {
+    return gulp.src(src)
+      .pipe(sourcemaps.init())
+      .pipe(babel({
+        presets: ['es2015']
+      }))
+      .on('error', function(error) {
+        console.log('error!');
+        console.log(error.message);
+        this.emit('end');
+      })
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(dst))
+      .pipe(connect.reload());
+  }
+}
+
+// ohm-konnakol
+gulp.task('babel', babelFactory(['src/*.js'], 'dst'));
 
 gulp.task('connect', function() {
   connect.server({ livereload: true });
@@ -27,4 +32,24 @@ gulp.task('watchjs', function() {
   gulp.watch(['src/*.js', 'assets/*.ohm'], ['babel']);
 });
 
+// chrome extension
+gulp.task('chrome-build', ['babel'], function() {
+  gulp.src(['node_modules/ohm-js/dist/ohm.js'])
+    .pipe(gulp.dest('chrome/extension/scripts'));
+
+  gulp.src(['dst/*.js'])
+    .pipe(gulp.dest('chrome/extension/scripts'));
+
+  gulp.src(['assets/**/*'])
+    .pipe(gulp.dest('chrome/extension/assets'));
+});
+
+gulp.task('chrome-babel',
+  babelFactory(['chrome/src/*.js'], 'chrome/extension/scripts'));
+
+gulp.task('chrome-watch', ['chrome-build'], function() {
+  gulp.watch('chrome/src/*.js', ['chrome-babel']);
+});
+
+// default
 gulp.task('default', ['watchjs', 'connect']);
