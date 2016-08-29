326355fc86c6bbc508ac3ad8ec6ba6f1b19fa65d
Arthur Carabott <arthurc@gmail.com>
2016-08-29 12:14:17 -0700

    adding playbuttons on browseraction

--- chrome/src/content.js
+++ chrome/src/content.js
@@ -1,18 +1,27 @@
 const port = chrome.runtime.connect();
 const key = '/konnakol';
-const konnakols = Array.from(document.body.querySelectorAll('*'))
-  .filter(node => (node.tagName.toUpperCase() !== 'SCRIPT') &&
-                   node.children.length === 0)
-  .map(node => {
-    let konnakol;
-    if (node.value !== undefined && node.value.includes(key)) {
-      konnakol = node.value;
-    }
-    else if (node.textContent.includes(key)) {
-      konnakol = node.textContent;
-    }
-    return konnakol;
-  })
-  .filter(node => node !== undefined);
+const konnakolNodes = Array.from(document.body.querySelectorAll('*'))
+  .filter(node => {
+    return node.tagName.toUpperCase() !== 'SCRIPT' && // no scripts
+           node.children.length === 0 &&              // no parent nodes
+           ((node.value !== undefined && node.value.includes(key)) || // has key
+            node.textContent.includes(key));
+  });
 
-port.postMessage(konnakols);
+function getKonnakolContent (element) {
+  return element.value === undefined ? element.textContent : element.value;
+}
+
+function addPlayButton (element) {
+  const button = document.createElement('input');
+  button.type = 'button';
+  button.value = 'play';
+  button.addEventListener('click', event => {
+    port.postMessage([getKonnakolContent(element)]);
+  });
+  element.parentElement.insertBefore(button, element.nextSibling);
+}
+
+konnakolNodes.forEach(konnakol => addPlayButton(konnakol));
+
+// port.postMessage(konnakols);
