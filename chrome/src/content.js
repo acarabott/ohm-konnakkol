e04955c1a987603e99eae29a1d42787b9dd4d05e
Arthur Carabott <arthurc@gmail.com>
2016-08-29 19:19:36 -0700

    buttons get replaced on re-parse

--- chrome/src/content.js
+++ chrome/src/content.js
@@ -1,48 +1,68 @@
 const port = chrome.runtime.connect();
 
-const konnakol = {};
-konnakol.key = '/konnakol';
+// this should live in a konnakol parser module
+const key = '/konnakol';
+const buttonClass = 'konnakol-play-button';
+const buttons = [];
 
-konnakol.parse = (parent=document.body, buttonAction) => {
-  const hasKonnakolKey = node => {
-    return (typeof node.value === 'string' && node.value.includes(konnakol.key)) ||
-            node.textContent.includes(konnakol.key);
-  };
+function hasKonnakolKey(node) {
+  return (typeof node.value === 'string' && node.value.includes(key)) ||
+          node.textContent.includes(key);
+}
 
-  const getNodeContent = node => {
-    // input elements will use value, other elements will use textContent
-    const content = node.value === undefined ? node.textContent : node.value;
-    return content.replace(konnakol.key, '');
-  };
+function getNodeContent(node) {
+  // input elements will use value, other elements will use textContent
+  const content = node.value === undefined ? node.textContent : node.value;
+  return content.replace(key, '');
+}
 
-  const createPlayButtonFor = node => {
-    const button = document.createElement('input');
-    button.type = 'button';
-    button.value = 'play';
+function createPlayButtonFor(node, buttonAction) {
+  const button = document.createElement('input');
+  button.type = 'button';
+  button.value = 'play';
+  button.classList.add(buttonClass);
 
-    let content = getNodeContent(node);
-    button.addEventListener('click', event => {
-      buttonAction(content, event);
+  let content = getNodeContent(node);
+  button.addEventListener('click', event => {
+    buttonAction(content, event);
+  });
+  node.parentElement.insertBefore(button, node.nextSibling);
+
+  if (['textarea', 'input'].includes(node.tagName.toLowerCase())) {
+    node.addEventListener('input', event => {
+      content = getNodeContent(node);
     });
-    node.parentElement.insertBefore(button, node.nextSibling);
+  }
+}
 
-    if (['textarea', 'input'].includes(node.tagName.toLowerCase())) {
-      node.addEventListener('input', event => {
-        content = getNodeContent(node);
-      });
-    }
-  };
-
-  const konnakolNodes = Array.from(parent.querySelectorAll('*'))
+function getKonnakolNodes(parent=document.body) {
+  return Array.from(parent.querySelectorAll('*'))
     .filter(node => {
       return node.tagName.toUpperCase() !== 'SCRIPT' && // no scripts
              node.children.length === 0 &&              // no parent nodes
              hasKonnakolKey(node);                      // has the key we want
     });
+}
 
-  konnakolNodes.forEach(node => createPlayButtonFor(node));
-};
+function addPlayButtons (arrayOfNodes, buttonAction) {
+  arrayOfNodes.forEach(node => createPlayButtonFor(node, buttonAction));
+}
 
-konnakol.parse(document.body, (text, event) => {
-  port.postMessage(text);
-});
+function removePlayButtons() {
+  for (const node of document.getElementsByClassName(buttonClass)) {
+    console.log('removing', node);
+    node.remove();
+  }
+}
+
+function parse(parent, buttonAction) {
+  removePlayButtons();
+  addPlayButtons(getKonnakolNodes(parent), buttonAction);
+}
+
+// end of konnakol parser module
+
+// 'main'
+const parent = document.body;
+const buttonAction = (text, event) => port.postMessage(text);
+parse(parent, buttonAction);
