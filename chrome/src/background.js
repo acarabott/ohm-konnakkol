779a1f6c221fd2c1881c0fa26739ecb8d9f71f00
Arthur Carabott <arthurc@gmail.com>
2016-08-29 18:55:53 -0700

    context menus for playback and parsing

--- chrome/src/background.js
+++ chrome/src/background.js
@@ -1,5 +1,5 @@
-chrome.browserAction.onClicked.addListener(tab => {
-  chrome.tabs.executeScript(tab.id, {
+function executeParse (tabId) {
+  chrome.tabs.executeScript(tabId, {
     file: "scripts/content.js",
     allFrames: true
   }, results => {
@@ -9,10 +9,47 @@ chrome.browserAction.onClicked.addListener(tab => {
       }
     });
   });
+}
+
+chrome.browserAction.onClicked.addListener(tab => {
+  executeParse(tab.id);
 });
 
 chrome.runtime.onConnect.addListener(port => {
   port.onMessage.addListener((message, sender, sendResponse) => {
     konnakol.play(message);
   });
+});
+
+chrome.contextMenus.removeAll();
+
+const menuOptions = [
+  {
+    args: {
+      id: 'play',
+      title: 'Konnakol: Play Selection',
+      contexts: ['selection']
+    },
+    action: (info, tab) => {
+      konnakol.play(info.selectionText);
+    }
+  },
+  {
+    args: {
+      id: 'parse',
+      title: 'Konnakol - add play buttons',
+      contexts: ['page']
+    },
+    action: (info, tab) => {
+      executeParse(tab.id);
+    }
+  }
+];
+
+menuOptions.forEach(options => {
+  chrome.contextMenus.create(options.args);
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  menuOptions.find(opts => opts.args.id === info.menuItemId).action(info, tab);
 });
