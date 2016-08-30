aa70c9992c61024e43f267ac3489428ffdb58fe5
Arthur Carabott <arthurc@gmail.com>
2016-08-30 11:43:00 -0700

    created konnakol editor class

---
+++ src/konnakol-editor.js
@@ -0,0 +1,63 @@
+ konnakol.Editor = class Editor {
+  constructor(id) {
+    this.key = '/konnakol';
+
+    this.container = document.createElement('div');
+    this.container.id = id;
+
+    // text area
+    this.selectionStart = 0;
+    this.selectionEnd = 0;
+
+    this.textarea = document.createElement('textarea');
+    this.textarea.rows = 2;
+    this.textarea.cols = 80;
+
+    this.textarea.addEventListener('focusin', event => {
+      event.target.selectionStart = this.selectionStart;
+      event.target.selectionEnd = this.selectionEnd;
+    });
+
+    this.textarea.addEventListener('focusout', event => {
+      this.updateSelection();
+    });
+
+    this.textarea.addEventListener('keydown', event => {
+      if (event.key === 'Enter' && event.metaKey) {
+        this.play();
+      }
+    });
+
+    this.container.appendChild(this.textarea);
+
+    // controls
+    this.controls = document.createElement('div');
+    this.container.appendChild(this.controls);
+
+    this.playButton = document.createElement('input');
+    this.playButton.type = 'button';
+    this.playButton.value = 'Play';
+    this.playButton.classList.add('button');
+
+    this.playButton.addEventListener('click', event => {
+      this.play();
+    });
+    this.controls.appendChild(this.playButton);
+  }
+
+  updateSelection() {
+    this.selectionStart = this.textarea.selectionStart;
+    this.selectionEnd = this.textarea.selectionEnd;
+  }
+
+  getContent() {
+    this.updateSelection();
+    return this.selectionEnd - this.selectionStart > 0 ?
+      this.textarea.value.slice(this.selectionStart, this.selectionEnd) :
+      this.textarea.value;
+  }
+
+  play() {
+    konnakol.play(this.getContent().replace(this.key, ''));
+  }
+};
